76e7e541c244ba02a197bab90b41bdb74aa9cb43
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-24 16:16:08 +0100

    'update'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      Hey Devs!
+      
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import { NavLinks } from "@/constants";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav>
-        <div className='nav-content'>
-            
-        </div>
-      
+      <div className="nav-content">
+        <Image src="/Logo.svg" alt="logo" width={50} height={50} />
+        {NavLinks.map((link) => (
+          <div className="nav-links" key={link.id}>
+            <Link href={link.url}>
+            {link.title}
+            </Link>
+          </div>
+        ))}
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
