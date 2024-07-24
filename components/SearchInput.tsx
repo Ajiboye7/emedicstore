cd73418b33d859a37f7289b9b11826a26c240334
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-24 18:49:47 +0100

    update

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 const SearchInput = () => {
   return (
-    <div className="relative w-full max-w-md mx-auto mt-10">
+    <div className="w-[580px] mt-10 relative">
       <input
         type="text"
         placeholder="Enter Product, Active Ingredient or Manufacturer's name"
         className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+      <div className="absolute  p-3 pointer-events-none left-[500px] top-[-10px]">
         <Image
           src="/Search Icon.svg"
           alt="Search"
