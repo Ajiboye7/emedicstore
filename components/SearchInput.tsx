5d41321a5f82f909a2994f7b7ece22e5baf60d99
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-24 18:57:33 +0100

    commit

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,3 +1,30 @@
+/*import React from 'react';
+import Image from 'next/image';
+
+const SearchInput = () => {
+  return (
+    <div className="w-[580px] mt-10 relative">
+      <input
+        type="text"
+        placeholder="Enter Product, Active Ingredient or Manufacturer's name"
+        className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <div className="absolute p-3 pointer-events-none left-[500px] top-[-10px] bg-black rounded-r-full">
+        <Image
+          src="/Search Icon.svg"
+          alt="Search"
+          width={40}
+          height={40}
+          className='bg-black'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;*/
+
+
 import React from 'react';
 import Image from 'next/image';
 
@@ -9,7 +36,7 @@ const SearchInput = () => {
         placeholder="Enter Product, Active Ingredient or Manufacturer's name"
         className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <div className="absolute  p-3 pointer-events-none left-[500px] top-[-10px]">
+      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex items-center bg-black p-2 rounded-r-full">
         <Image
           src="/Search Icon.svg"
           alt="Search"
@@ -23,3 +50,4 @@ const SearchInput = () => {
 };
 
 export default SearchInput;
+
