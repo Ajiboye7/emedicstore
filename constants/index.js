bed92cb203037ae4fa0e621b96d263070152c334
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-29 18:54:05 +0100

    upate

--- constants/index.js
+++ constants/index.js
@@ -4,18 +4,6 @@ export const NavLinks =[
         title:'Company',
         url: '/'
     },
-    {
-        id: 2,
-        title:'Products',
-        url: '/'
-    },
-    {
-        id: 3,
-        title:'Services',
-        url: '/'
-    },
-    {
-        id: 4,
         title:'Services',
         url: '/'
     },
