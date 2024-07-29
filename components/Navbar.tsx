3dddd39166504b1a900cc5ef4604f9dc8b5dbca9
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-29 18:50:36 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,14 @@ const Navbar = () => {
             title="Become a vendor"
             buttonStyle="bg-secondary-button border-2 border-solid border-blue-500 px-4 py-2"
             iconStyle=""
-            Icon=
+            Icon="/SVG.svg"
+          />
+          <CustomButton
+            title="Sign Up"
+            buttonStyle="bg-primary-button px-4 py-2"
+            iconStyle=""
+            Icon="/SVG2.svg"
+          />
           
           <div className="relative">
             <Image
