e04b9b4cbe76c60d251aa5eef249985af7dff0cc
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-29 18:53:15 +0100

    update

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -35,18 +35,7 @@ const SearchInput = () => {
         type="text"
         placeholder="Enter Product, Active Ingredient or Manufacturer's name"
         className="w-full h-[50px] pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex items-center bg-black rounded-r-full h-full p-5">
-        <Image
-          src="/Search Icon.svg"
-          alt="Search"
-          width={20}
-          height={20}
-          className=''
-          /*right-0 top-1/2 transform -translate-y-1/2 */
-        />
-      </div>
-    </div>
+
   );
 };
 
