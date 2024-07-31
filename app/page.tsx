82ce8ee0bba84f0e06fb602a89231dddb3444db6
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-31 22:15:14 +0100

    'update'

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Header, Partners, Perks } from "@/components";
+export default function Home() {
+  return (
+    <main>
+      <Header/>
+      <Partners/>
+      <Perks/>
+    </main>
+  );
+}
