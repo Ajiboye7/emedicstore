5360dead4f2980ec94399594d0061a0b427c3eea
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-31 22:13:56 +0100

    'update'

---
+++ components/Partners.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+const Partners = () => {
+  return (
+    <section>
+      <div className="max-w-[1200px] mx-auto border border-solid border-blue-500">
+        <p className="text-center text-bodyText">
+          TRUSTED & ENDORSED BY INDUSTRY PROFESSIONALS
+        </p>
+        <div className="partners flex items-center justify-center gap-6 md:gap-10 lg:gap-20 mt-10 mx-5 flex-wrap">
+          <Image src="/NAFDAC.svg" alt="nafdac" width={270} height={70} />
+          <Image src="/PCN.svg" alt="pcn" width={270} height={70} />
+          <Image src="/Group.svg" alt="paystack" width={270} height={60} />
+          <Image src="/Zenith.svg" alt="zenith" width={70} height={40} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Partners;
