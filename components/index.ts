4ed346670585000d12db58aeea585d43ffa6c9e5
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-31 22:12:25 +0100

    'update'

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,18 @@
- 
+import Navbar from './Navbar'
+import SearchInput from './SearchInput'
+import CustomButton from './CustomButton'
+import Header from './Header'
+import GradientText from './TextTitle'
+import Partners from './Partners'
+import Perks from './Perks'
+
+export{
+    Navbar,
+    SearchInput,
+    CustomButton,
+    Header,
+    GradientText,
+    Partners,
+    Perks
+}
+
