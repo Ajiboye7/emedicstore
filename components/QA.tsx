caffc36416309b8c5a647ac218530c64f495b3f3
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-03 17:24:55 +0100

    'update'

--- components/QA.tsx
+++ components/QA.tsx
@@ -1,3 +1,5 @@
+
+'use client'
 import React from 'react';
 import TextTitle from './TextTitle';
 import { useState } from 'react';
@@ -9,27 +11,30 @@ const QA = () => {
     setSelectedQuestionId(id);
   }
 
-  const selectedQA =  QuestionAnswer.find(qa => qa.id === selectedQuestionId)
-  const selectedAnswer = selectedQA ? selectedQA.answer : 'Click question to display answer.'
+  const selectedQuestion =  QuestionAnswer.find(qa => qa.id === selectedQuestionId)
+  const selectedAnswer = selectedQuestion ? selectedQuestion.answer : 'Click question to display answer.'
   return (
     <section>
-      <div>
+      <div className='max-w-[1800px] mx-auto'>
         <p>Things YOU MAY WANT TO KNOW</p>
         <TextTitle>Got Questions?</TextTitle>
-        <div className='question'>
+        <div className='flex gap-20 mx-auto max-h-[700px]'>
+        <div className='question flex flex-col gap-8 flex-1 max-w-[800px] overflow-y-scroll max-h-full hide-scrollbar'>
           {QuestionAnswer.map(qa=>(
             <button
             key={qa.id}
-             className= {`text-left ${qa.id === selectedQuestionId ? 'bg-blue-100' : ''}`}
+            className={`p-8 border border-primaryButtonBg rounded-3xl text-left text-2xl  ${qa.id === selectedQuestionId ? 'bg-blue-100' : ''}`}
              onClick={() => handleQuestionClick(qa.id)}
             >
               {qa.question}
             </button>
           ))}
         </div>
-        <div className='answer'>
-
+        <div className='answer text-white font-semibold border rounded-3xl flex flex-1 items-center justify-center max-w-[800px] p-10 text-3xl text-center leading-relaxed bg-gradient-to-r from-primaryGradientEnd to-primaryGradientStart'>
+          <p>{selectedAnswer}</p>
         </div>
+        </div>
+        
       </div>
     </section>
   );
--- components/QandA.tsx
+++ components/QandA.tsx
@@ -39,7 +39,7 @@ const QandA = () => {
         {questionsAnswers.map(qa => (
           <button
             key={qa.id}
-            className={`p-4 border rounded-lg text-left ${qa.id === selectedQuestionId ? 'bg-blue-100' : ''}`}
+            className={`p-4 border rounded-lg text-left ${qa.id === selectedQuestionId ? 'bg-primaryButtonBg text-white' : ''}`}
             onClick={() => handleQuestionClick(qa.id)}
           >
             {qa.question}
