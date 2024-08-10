34320fdbd87f4d0756e075a4024f535c8507ed15
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-10 22:10:41 +0100

    'updadate'

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -44,52 +44,7 @@ export default GetStarted;
 
 {/*
     
-    import React from 'react';
-import TextTitle from './TextTitle';
-import AppButton from './AppButton';
-import Image from 'next/image';
-
-const GetStarted = () => {
-  return (
-    <section className="max-w-[1440px] mx-auto p-10 pb-0 pt-24 gradient-header rounded-3xl">
-      <div className="flex flex-col max-w-[1440px] mx-auto">
-        <div className="flex flex-col justify-center items-center gap-10 mb-8">
-          <TextTitle textStyle="text-4xl md:text-5xl lg:text-7xl text-center">
-            Get Our <span className="font-bold">App</span> Today
-          </TextTitle>
-          <p className="w-full md:w-[700px] text-center text-xl md:text-2xl text-bodyText leading-relaxed">
-            Experience healthcare at your fingertips—get our app for easy access to quality healthcare anytime, anywhere.
-          </p>
-        </div>
-        <div className="flex flex-col lg:flex-row justify-around items-center gap-10">
-          <Image
-            src="/Samsung Galaxy S20 5G.svg"
-            alt="screen"
-            width={300}
-            height={300}
-            className="w-60 h-60 md:w-72 md:h-72 lg:w-80 lg:h-80"
-          />
-          <div className="flex flex-col md:flex-row gap-10">
-            <AppButton
-              title="Get it on<br> Google Play"
-              Icon="/Icon (1).svg"
-              imageStyle="w-5 md:w-8 lg:w-10 xl:w-16 sm:ml-2"
-              containerStyles="flex-row-reverse bg-customBg"
-              line2Styles="font-bold md:text-3xl lg:text-4xl"
-              line1Styles="md:text-2xl lg:text-2xl md:mr-16 lg:mr-24"
-              textStyles="text-white"
-            />
-            <AppButton
-              title="Download on the<br> App Store"
-              Icon="/Icon.svg"
-              imageStyle="w-5 md:w-8 lg:w-10 xl:w-16 sm:ml-2"
-              containerStyles="flex-row-reverse bg-customBg"
-              line2Styles="font-bold md:text-3xl lg:text-4xl"
-              line1Styles="text-1xl md:text-2xl"
-              textStyles="text-white"
-            />
-          </div>
-          <Image
+ 
             src="/Space Black.svg"
             alt="screen"
             width={300}
