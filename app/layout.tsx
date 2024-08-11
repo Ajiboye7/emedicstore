95e6c7ea7d8a76b44476d5c052a812f48c79dfc7
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-11 22:07:45 +0100

    'updte'

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
-    </html>
+   
   );
 }
