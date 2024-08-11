0733c5fe44dd49538eecfc361e39647fc308649d
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-11 22:05:30 +0100

    'update'

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -9,7 +9,12 @@ const GetStarted = () => {
       <div className='flex flex-col max-w-[1440px] mx-auto'>
         <div className='flex flex-col justify-center items-center gap-12 mb-8 '>
         <TextTitle textStyle=' text-2xl sm:text-4xl md:text-6xl xl:text-7xl '>Get Our <span className='font-bold'> App</span> Today</TextTitle>
-      
+        <p className='md:w-[700px] text-center sm:text-lg md:text-xl xl:text-2xl text-bodyText leading-relaxed'>Experience healthcare at your fingertips—get our app for easy access to quality healthcare anytime, anywhere.</p>
+        </div>
+        <div className='flex lg:flex-row flex-col gap-10 justify-around items-center'>
+            <Image src= '/Samsung Galaxy S20 5G.svg' alt='screen'  width={300} height={300}/>
+            <div className='flex gap-5 flex-wrap justify-center items-center'>
+            <AppButton 
             title="Get it on<br> Google Play"
             Icon="/Icon (1).svg"
             imageStyle="w-4 md:w-8 lg:w-10 xl:w-16 sm:ml-2"
