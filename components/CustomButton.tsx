18308b0df013d6a144f59a0c66bc2d73667e4920
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-20 22:04:52 +0100

    'late'

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,7 +5,11 @@ import { CustomButtonProps } from '@/types';
 const CustomButton = ({ title, buttonStyle,iconStyle,Icon,titleStyle}: CustomButtonProps) => {
   return (
     <div>
-      <button type='submit' class
+      <button type='submit' className={`flex items-center gap-2 rounded-full whitespace-nowrap ${buttonStyle}`}>
+        <span className={`${titleStyle} btn-text`}>{title}</span>
+        <Image 
+          alt='buttonIcon'
+          src={Icon}
           width={10}
           height={10}
           className={`${iconStyle}`}
