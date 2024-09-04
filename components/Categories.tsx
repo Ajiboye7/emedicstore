5ace08acfc50474a89dc12ed6e6588053b6e00c3
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-04 19:42:13 +0100

    'update'

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -49,7 +49,40 @@ export default Categories;
 
 
 
+{/*
+    import React from "react";
+import { HealthCategories } from "@/constants";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+import TextTitle from "./TextTitle";
 
+const Categories = () => {
+  return (
+    <section className="py-16">
+      <div className="container max-w-[1900px] mx-auto w-full px-4">
+        <div className="text-center">
+          <p className="text-bodyText">OUR BEST-SELLING CATEGORIES</p>
+          <TextTitle textStyle="text-7xl my-10">Your Go-To Categories</TextTitle>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {HealthCategories.map((cat) => (
+            <div
+              key={cat.id}
+              className="p-6 shadow-md mx-auto rounded-3xl"
+              style={{
+                border: `1px solid ${cat.borderColor}`,
+              }}
+            >
+              <p className="text-4xl text-categoryTextTitle font-semibold">{cat.title}</p>
+              <Image
+                src={cat.img}
+                alt={cat.alt}
+                width={500}
+                height={500}
+                className="my-8 mx-auto"
+              />
+              <div className="button">
+                <CustomButton
                   title="Explore now"
                   buttonStyle="bg-secondary-button border-2 border-solid border-blue-500 px-5 py-4"
                   iconStyle=""
