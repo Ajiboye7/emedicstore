a996a98bbef54f0fab1cb8b29ef9bca2a3c40500
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-04 19:38:23 +0100

    'input'

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -22,35 +22,7 @@ const SearchInput = () => {
   );
 };
 
-export default SearchInput;*/
 
-
-/*import React from 'react';
-import Image from 'next/image';
-
-const SearchInput = () => {
-  return (
-    <div className="w-[580px] relative">
-      <input
-        type="text"
-        placeholder="Enter Product, Active Ingredient or Manufacturer's name"
-        className="w-full h-[50px] pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex items-center bg-black rounded-r-full h-full p-5">
-        <Image
-          src="/Search Icon.svg"
-          alt="Search"
-          width={20}
-          height={20}
-          className=''
-          /*right-0 top-1/2 transform -translate-y-1/2 
-        />
-      </div>
-    </div>
-  );
-};
-
-export default SearchInput;*/
 import React from 'react';
 import Image from 'next/image';
 
