04f4d025b4a2552c88f2d425ac2aeede6b222606
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-11 16:57:12 +0100

    index

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,22 @@
 export interface CustomButtonProps{
     title: String
-    bu
+    buttonStyle:string
+    iconStyle: string
+    Icon:string
+    titleStyle: string
+}
+
+export interface TextTitleProps{
+    textStyle?:string
+    children?: React.ReactNode;
+}
+export interface AppButtonProps{
+    title: string
+    containerStyles ?: string
+    textStyles?: string
+    Icon: string
+    imageStyle?:string
+    line1Styles?:string
+    line2Styles? :string
 }
 
